41bd7646567c270fdd812364d11ac8962f937e5f
betich <betich365@gmail.com>
2024-12-24 02:18:47 +0700

    ci(lint): add eslint typescript config to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,10 @@ import tseslint from "typescript-eslint"
 export default tseslint.config(
   // add more generic rule sets here, such as:
   js.configs.recommended,
+  tseslint.configs.recommended,
   eslintPluginAstro.configs.recommended,
   {
+    files: ["**/*.astro, **/*.tsx, **/*.jsx, **/*.ts, **/*.js"],
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
